944b538ef13cb37e0c7a073177646165a2145fe6
ruitao.xu <ruitao.xu@aliyun.com>
2018-01-23 17:14:18 +0800

    update lib_common.sh

--- bash/lib_common.sh
+++ bash/lib_common.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# quit when any error happens
+set -e
+# quit when use undefined variable
+set -u
+# quit when error happens in pipe
+set -o pipefail
+trap "echo 'error: Script failed: see failed command above'" ERR
+
 readonly ABSPath=$(cd $(dirname $0); pwd)
 
 usage()
